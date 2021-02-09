2331644395e7e1b145b6f5a574e38472fc1814fc
Youssef <youssefabdullah235@gmail.com>
2021-02-09 15:09:55 +0200

    init

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+
+
+namespace WebApplication1.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private Training_CenterEntities4 db = new Training_CenterEntities4();
+        private Training_CenterEntities5 db2 = new Training_CenterEntities5();
+        private Training_CenterEntities6 db3 = new Training_CenterEntities6();
+        public string Email ="";
+        public int Id ;
+        public ActionResult Index()
+        {
+            return View(db.Course.ToList());
+        }
+
+        public ActionResult About(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Course course = db.Course.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course);
+        }
+        public ActionResult Profile()
+        {
+            return View(db3.UserCourse.ToList());
+        }
+        int i = 1;
+        public ActionResult Book( int? courseId, string CourseDes, string CourseImg, int? CoursePrice,string CourseName)
+        {
+            UserCourse table = new UserCourse();
+            
+            table.UserId = Id;
+            table.CourseID = (int)courseId ;
+            table.CourseDes = CourseDes;
+            table.CourseImg = CourseImg;
+            table.CoursePrice= CoursePrice;
+            table.CourseName= CourseName;
+            if (ModelState.IsValid)
+            {
+                db3.UserCourse.Add(table);
+                db3.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        [ActionName("Contact")]
+        public ActionResult Post_Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpGet]
+        [ActionName("Contact")]
+        public ActionResult Get_Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult LogOut()
+        {
+            Email = "";
+            Session["Email"] = Email;
+            Session["togle"] = "f";
+            return RedirectToAction("Index");
+        }
+
+        [ActionName("SignIn")]
+        [HttpGet]
+        public ActionResult SignIn_get()
+        {
+            ViewBag.n1 = "";
+            ViewBag.n2 = "";
+            ViewBag.r = "";
+            return View();
+        }
+
+        [ActionName("SignIn")]
+        [HttpPost]
+        public ActionResult SignIn_post(string n1 , string n2)
+        {
+            string sum = n1 + n2;
+            ViewBag.r = sum;
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp([Bind(Include = "Id,UserEmail,UserPassword")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (user.UserEmail != "" && user.UserPassword !="")
+                {
+                    Email = user.UserEmail;
+                    Id = user.Id;
+                    if(Email == "youssef@gmail.com")
+                    {
+                        Session["admin"] = "t";
+                    }
+                    else
+                    {
+                        Session["admin"] = "f";
+                    }
+                    Session["Email"] = Email;
+                    Session["togle"] = "t";
+                    db.User.Add(user);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                
+            }
+
+            return View(user);
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login([Bind(Include ="UserEmail,UserPassword")]User user)
+        {
+            var rec = db.User.Where(x => x.UserEmail == user.UserEmail && x.UserPassword == user.UserPassword).ToList().FirstOrDefault();
+            
+            if (rec != null)
+            {
+                Email = user.UserEmail;
+                Id = user.Id;
+                if (Email == "youssef@gmail.com")
+                {
+                    Session["admin"] = "t";
+                }
+                else
+                {
+                    Session["admin"] = "f";
+                }
+
+                ViewBag.error = "";
+                
+                Session["Email"] = Email;
+                Session["togle"] = "t";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.error = "Invalid User";
+                return View(user);
+            }
+            
+        }
+        
+    }
+}
---
+++ WebApplication1/Controllers/UserCoursesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserCoursesController : Controller
+    {
+        private Training_CenterEntities6 db = new Training_CenterEntities6();
+
+        // GET: UserCourses
+        public ActionResult Index()
+        {
+            return View(db.UserCourse.ToList());
+        }
+
+        // GET: UserCourses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserCourse userCourse = db.UserCourse.Find(id);
+            if (userCourse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userCourse);
+        }
+
+        // GET: UserCourses/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserCourses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserId,CourseID,CourseName,CourseImg,CourseDes,CoursePrice")] UserCourse userCourse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserCourse.Add(userCourse);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userCourse);
+        }
+
+        // GET: UserCourses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserCourse userCourse = db.UserCourse.Find(id);
+            if (userCourse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userCourse);
+        }
+
+        // POST: UserCourses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserId,CourseID,CourseName,CourseImg,CourseDes,CoursePrice")] UserCourse userCourse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userCourse).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userCourse);
+        }
+
+        // GET: UserCourses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserCourse userCourse = db.UserCourse.Find(id);
+            if (userCourse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userCourse);
+        }
+
+        // POST: UserCourses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserCourse userCourse = db.UserCourse.Find(id);
+            db.UserCourse.Remove(userCourse);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
